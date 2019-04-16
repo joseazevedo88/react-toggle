95063c59a78cf30144896102aa3f37be6d2d62bc
Zé Azevedo <zeazevedo@zes-macbook-pro.ginetta.local>
2019-04-16 14:01:24 +0200

    Complete initial layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Toggle test</h1>
-      </div>
-    );
-  }
+const Input = styled.input`
+  height: 20rem;
+  width: 40rem;
+  border-radius: 10rem;
+  appearance: none;
+  background: lightsalmon;
+`;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  background: lightblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Input type="checkbox" />
+    </Wrapper>
+  );
 }
 
 export default App;
